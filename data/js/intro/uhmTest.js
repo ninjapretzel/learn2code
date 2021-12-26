567270d00ed2253c25d788b20865b2451c6cdca1
ninjapretzel <ninjapretzel@gmail.com>
2021-12-26 15:26:41 -0700

    includes formatting in test lesson

--- data/js/intro/uhmTest.js
+++ data/js/intro/uhmTest.js
@@ -5,9 +5,9 @@ export default {
 	Content: {
 		Category: "Intro",
 		Lesson: "Hello World", 	
-		LessonText: "The first thing any programmer does is make their computer say "
-			+ "\"Hello, World!\"."
-			+ "Use the `print()` function to do that.",
+		LessonText: "The first thing any programmer does is make their computer say"
+			+ "\n\"Hello, World!\"."
+			+ "\nUse the print(\"stuff\") function",
 		DocLinks: [""]
 	},
 	Preamble: "function hello() {",
