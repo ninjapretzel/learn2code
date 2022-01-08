d1099ddf5af4ae44e9d3dc03019c84e0417de161
ninjapretzel <ninjapretzel@yahoo.com>
2022-01-08 15:49:20 -0700

    Makes the browse template have some directions

--- src/templates/Browse.js
+++ src/templates/Browse.js
@@ -6,7 +6,9 @@ class Browse extends Template {
 		for (let cat in data.Categories) {
 			const category = data.Categories[cat][lang];
 			const lessons = [];
+			let i = 0;
 			for (let idx in category) {
+				i++;
 				const lesson = category[idx]
 				console.log(lesson);
 				const catName = cat;
@@ -14,17 +16,18 @@ class Browse extends Template {
 					GLOBALS.showLesson(data.Categories[catName][GLOBALS.LANG][idx]);
 				}
 				lessons.push(<div id="" className="col s12 card blue-grey lighten-1">
-					<h5 onClick={loader}><a> {lesson.Content.Lesson} </a></h5>
+					<h5 onClick={loader}>{i}. {lesson.Content.Lesson} </h5>
 				</div>);
 			}
 			inner.push(<div className="col s12 card blue-grey">
-				<h3>{cat}</h3>
+				<h3>Category: {cat}</h3>
 				<div className="row">
 					{lessons}
 				</div>
 			</div>);
 		}
 		return <div className="col s12 row rowfix">
+			<div className="col s12"><h6>Click a lesson name to load it</h6></div>
 			{inner}
 		</div>	
 	}
