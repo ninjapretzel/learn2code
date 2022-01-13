830ce8a919add9ee10720d848c2c9dff32dc76dc
ninjapretzel <ninjapretzel@yahoo.com>
2022-01-13 15:32:43 -0700

    Comments some spammy logs

--- src/pyVm.js
+++ src/pyVm.js
@@ -37,10 +37,10 @@ const preNewlines = count(pre, "\n");
 export async function exec(code, injected) {
 	for (let name in injected) {
 		pyodide.globals.set(name, injected[name]);
-		console.log("Python Injected " + name);
+		//console.log("Python Injected " + name);
 	}
 	code = pre + code;
-	console.log("Running the following code:\n",code);
+	//console.log("Running the following code:\n",code);
 	pyodide.runPython(code);
 }
 export async function extract() {
@@ -51,7 +51,7 @@ export async function onError(scriptEntry, error) {
 	const str = error.toString();
 	const search = "File \"<exec>\", line ";
 	let idx = str.lastIndexOf(search);
-	console.log("pyvm.onError: index at", idx, "in", str);
+	// console.log("pyvm.onError: index at", idx, "in", str);
 	idx += search.length;
 	let num = ""
 	while (str[idx] >= '0' && str[idx] <= '9') {
@@ -74,13 +74,12 @@ export async function onError(scriptEntry, error) {
 	 // I have no idea why this needs to be adjusted by an additional 2...
 	num = Number(num) - preNewlines - 2;
 	
-	console.log("Got line #", num+ "\nline=\""+ line + "\"\nmark=\""+ marker+ "\"\nerr ="+ err);
+	// console.log("Got line #", num+ "\nline=\""+ line + "\"\nmark=\""+ marker+ "\"\nerr ="+ err);
 	M.toast({html:`Script Error on line # ${num}.<br /> ${err}`, classes:"red" });
 	let startMark = { line: num, col: 0 }
 	let endMark = { line: num+1, col: 0 }
 	
-	console.log("Marking from", startMark, "to", endMark);
-	GLOBALS.clearMarkers()
+	// console.log("Marking from", startMark, "to", endMark);
 	GLOBALS.markText(startMark, endMark, { css: "color: #faa;background: #a33" });
 	
 }
